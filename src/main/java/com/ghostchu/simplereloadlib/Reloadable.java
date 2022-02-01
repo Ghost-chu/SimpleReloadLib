38c5b471077a6c6563e3d11afc6c59b2be80e98b
Ghost_chu <2908803755@qq.com>
2022-02-01 16:14:00 +0800

    补充javadoc，递增版本号

--- src/main/java/com/ghostchu/simplereloadlib/Reloadable.java
+++ src/main/java/com/ghostchu/simplereloadlib/Reloadable.java
@@ -8,7 +8,7 @@ public interface Reloadable {
      * Callback for reloading
      *
      * @return Reloading success
-     * @throws Exception Throws error if module failed to process reloading
+     * @throws Exception Throws error if module failed to process reloading (this will turn ReloadStatus to ReloadStatus.EXCEPTION)
      */
     default ReloadResult reloadModule() throws Exception {
         return ReloadResult.builder().status(ReloadStatus.SUCCESS).build();
